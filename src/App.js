de4504505e6ecd6da1cce08566c7c77bf8cda5b5
Boussif Imen <147039183+imenbf23@users.noreply.github.com>
2024-10-23 22:47:40 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 // src/App.js
 import React from 'react';
 import Register from './Register';
-
+import login from './login';
 function App() {
   return (
     <div className="App">
       <Register />
+      <login />
     </div>
   );
 }
